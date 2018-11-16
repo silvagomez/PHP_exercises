b41efa540caf9c903aef93b18ac1b331bcfaa410
Diego Camilo Silva Gómez <silva.camilo@gmail.com>
2018-11-16 22:37:16 +0100

    Update if_simplificado.php

--- tema3/if_switch/if_simplificado.php
+++ tema3/if_switch/if_simplificado.php
@@ -6,7 +6,4 @@ echo $n1 < $n2 ? "menor <br>":"mayor <br>";
 echo '$n2 < $n1 ? "menor":"mayor" <br>';
 echo $n2 < $n1 ? "menor <br>":"mayor <br>";
 
-$a=$n1 < $n2  ? return $o="menor": return $o="mayor";
-echo $a;
-
 ?>
