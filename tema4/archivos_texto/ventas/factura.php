a127435fbcad943be9bdfe9c4a2abd7053abfe0c
silvagomez <silva.camilo@gmail.com>
2018-12-03 19:19:36 +0100

    Trabajando

--- tema4/archivos_texto/ventas/factura.php
+++ tema4/archivos_texto/ventas/factura.php
@@ -9,22 +9,25 @@ $campo_venta=[];
 $campo_cliente=[];
 $fecha=date('j/n/Y');
 while (!feof($archivo_venta)) {
-    $linea_c=fgets($archivo_cliente);
+    $linea_cliente=fgets($archivo_cliente);
     utf8_encode($linea_venta);
-    utf8_encode($linea_c);
-    $campo_venta["venta".$contador_venta]=explode("|",$linea_venta);
-    $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_c);
+    utf8_encode($linea_cliente);
+    
 //     echo <<<abc
 //     {$campo_venta["venta".$contador_venta][0]} {$campo_cliente["cliente".$contador_cliente][0]}
 // abc;
     // $contador_cliente=1;
     //-//
-    while ($campo_venta["venta".$contador_venta][0] != $campo_cliente["cliente".$contador_cliente][0]) {
-        $linea_c=fgets($archivo_cliente);
-        $contador_cliente++;
-        // $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_c);
-        echo "son distintos";
+    // while ($campo_venta["venta".$contador_venta][0] != $campo_cliente["cliente".$contador_cliente][0]) {
+    //     $linea_cliente=fgets($archivo_cliente);
+    //     $contador_cliente++;
+    //     // $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_cliente);
+    //     echo "son distintos";
+    // }
+    while ((substr($linea_venta,0,2))!=(substr($linea_cliente,0,2))) { // pregunto por los tres primero carácteres
+        $linea_cliente=fgets($archivo_cliente);
     }
+    $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_cliente);
     //--//
     echo <<<abc
     Fecha: $fecha <br>
@@ -32,7 +35,8 @@ while (!feof($archivo_venta)) {
     <p>Código artículo---Descripción---Cantidad---Precio---Importe</p>
 abc;
     //-//
-    while ((!feof($archivo_venta)) && ($campo_venta["venta".$contador_venta][0] == $campo_cliente["cliente".$contador_cliente][0])) {
+    while ((!feof($archivo_venta)) && (!feof($archivo_cliente)) && (substr($linea_venta,0,2))==(substr($linea_cliente,0,2))) {
+        $campo_venta["venta".$contador_venta]=explode("|",$linea_venta);
         echo <<<abc
         {$campo_venta["venta".$contador_venta][1]}---{$campo_venta["venta".$contador_venta][2]}---{$campo_venta["venta".$contador_venta][4]}---{$campo_venta["venta".$contador_venta][3]}
         <br>
@@ -41,7 +45,7 @@ abc;
         $contador_venta++; 
     }
     // echo $linea_venta."<br>";
-    // $contador_cliente++;
+    $contador_cliente++;
     // $contador_cliente=1;
 }
 fclose($archivo_venta);
