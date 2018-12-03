9fe9153b8694be0ed8fdb2c1b8fc60b9651890f7
silvagomez <silva.camilo@gmail.com>
2018-12-03 20:01:59 +0100

    Trabajando
    Solucionado ejercicio de venta con su factura

--- tema4/archivos_texto/ventas/factura.php
+++ tema4/archivos_texto/ventas/factura.php
@@ -8,51 +8,48 @@ $contador_cliente=1;
 $campo_venta=[];
 $campo_cliente=[];
 $fecha=date('j/n/Y');
-while (!feof($archivo_venta)) {
+$total=0;
+while ((!feof($archivo_venta)) && (!feof($archivo_cliente))) { // pregunta sí no es fin de fichero
     $linea_cliente=fgets($archivo_cliente);
     utf8_encode($linea_venta);
     utf8_encode($linea_cliente);
-    
-//     echo <<<abc
-//     {$campo_venta["venta".$contador_venta][0]} {$campo_cliente["cliente".$contador_cliente][0]}
-// abc;
-    // $contador_cliente=1;
     //-//
-    // while ($campo_venta["venta".$contador_venta][0] != $campo_cliente["cliente".$contador_cliente][0]) {
-    //     $linea_cliente=fgets($archivo_cliente);
-    //     $contador_cliente++;
-    //     // $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_cliente);
-    //     echo "son distintos";
-    // }
-    while ((substr($linea_venta,0,2))!=(substr($linea_cliente,0,2))) { // pregunto por los tres primero carácteres
+    while ((substr($linea_venta,0,3))!=(substr($linea_cliente,0,3))) { // pregunto por los tres primero carácteres
         $linea_cliente=fgets($archivo_cliente);
     }
-    $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_cliente);
+    // echo substr($linea_venta,0,3)."<br>";
+    // echo substr($linea_cliente,0,3)."<br>";
+    $campo_cliente["cliente".$contador_cliente]=explode("|",$linea_cliente); // hago un explode para que esa linea_cliente se vuelva un array y se alamcene dentro del array campo_cliente ->OJO ES BIDIMENSIONAL
     //--//
+    //Imprimo la info necesaria del array bidimesional campo_cliente
     echo <<<abc
-    Fecha: $fecha <br>
-    Código de cliente: {$campo_cliente["cliente".$contador_cliente][0]} Nombre: {$campo_cliente["cliente".$contador_cliente][1]} <br>
-    <p>Código artículo---Descripción---Cantidad---Precio---Importe</p>
+    <h2>Fecha: $fecha <br>
+    Código de cliente: {$campo_cliente["cliente".$contador_cliente][0]} Nombre: {$campo_cliente["cliente".$contador_cliente][1]}</h2>
+    <b>Código artículo-----Descripción-----Cantidad-----Precio</b>
 abc;
     //-//
-    while ((!feof($archivo_venta)) && (!feof($archivo_cliente)) && (substr($linea_venta,0,2))==(substr($linea_cliente,0,2))) {
-        $campo_venta["venta".$contador_venta]=explode("|",$linea_venta);
+    while ((!feof($archivo_venta)) && (!feof($archivo_cliente)) && (substr($linea_venta,0,3))==(substr($linea_cliente,0,3))) { // sí no es fin de ficheros y sí los códigos son iguales
+        $campo_venta["venta".$contador_venta]=explode("|",$linea_venta); // hago un explode para que esa linea_venta se vuelva un array y se alamcene dentro del array campo_venta ->OJO ES BIDIMENSIONAL
+        //Imprimo la info necesaria del array bidimesional campo_venta
         echo <<<abc
-        {$campo_venta["venta".$contador_venta][1]}---{$campo_venta["venta".$contador_venta][2]}---{$campo_venta["venta".$contador_venta][4]}---{$campo_venta["venta".$contador_venta][3]}
+        {$campo_venta["venta".$contador_venta][1]}-----{$campo_venta["venta".$contador_venta][2]}-----{$campo_venta["venta".$contador_venta][4]}-----{$campo_venta["venta".$contador_venta][3]}
         <br>
 abc;
+        $total+=$campo_venta["venta".$contador_venta][3]; // voy sumando los precios
         $linea_venta=fgets($archivo_venta);
         $contador_venta++; 
     }
-    // echo $linea_venta."<br>";
+    echo <<<abc
+    <b>Total: $total</b><br><br>
+abc;
+    $total=0;
     $contador_cliente++;
-    // $contador_cliente=1;
 }
 fclose($archivo_venta);
 fclose($archivo_cliente);
-echo "<br><br>";
-print_r($campo_venta);
-echo "<br><br>";
-print_r($campo_cliente);
+echo "<br>";
+// print_r($campo_venta);
+echo "<br>";
+// print_r($campo_cliente);
 require_once '../../../estilo/formato_f.php';
 ?>
